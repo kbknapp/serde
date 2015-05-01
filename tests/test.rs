531f00dd98c3bdb2fedb3d9671eb0ae9e056c52d
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2015-05-01 10:17:30 -0700

    wip

--- tests/test.rs
+++ tests/test.rs
@@ -1,5 +1,12 @@
-#![cfg(feature = "syntex")]
-include!(concat!(env!("OUT_DIR"), "test.rs"));
+#![feature(test)]
+#![cfg_attr(not(feature = "syntex"), feature(custom_attribute, custom_derive, plugin))]
+#![cfg_attr(not(feature = "syntex"), plugin(serde_macros))]
+
+extern crate test;
+extern crate serde;
 
 #![cfg(feature = "syntex")]
 include!(concat!(env!("OUT_DIR"), "test.rs"));
+
+#![cfg(not(feature = "syntex"))]
+include!("test.rs.in");
