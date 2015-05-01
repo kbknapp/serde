31e61a7d3fd9b4bd44c6639a441c47500ca78254
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2015-05-01 07:58:49 -0700

    wip: Add syntex support

--- serde_codegen/src/lib.rs
+++ serde_codegen/src/lib.rs
@@ -1,10 +1,26 @@
-#![feature(custom_derive, rustc_private, unboxed_closures)]
-#![plugin(quasi_macros)]
+#![cfg_attr(not(feature = "syntex"), feature(custom_derive, rustc_private, unboxed_closures))]
+#![cfg_attr(not(feature = "syntex"), plugin(quasi_macros))]
 
 extern crate aster;
 extern crate quasi;
+
+#[cfg(feature = "syntex")]
+extern crate syntex;
+
+#[cfg(feature = "syntex")]
+extern crate syntex_syntax as syntax;
+
+#[cfg(not(feature = "syntex"))]
 extern crate syntax;
 
-mod ser;
-mod de;
-mod field;
+#[cfg(feature = "syntex")]
+include!(concat!(env!("OUT_DIR"), "lib.rs"));
+
+#[cfg(not(feature = "syntex"))]
+include!("lib.rs.in"));
+
+#[cfg(feature = "syntex")]
+pub fn register(reg: &mut syntex::Registry) {
+    reg.register_decorator("derive_Serialize", ser::expand_derive_serialize);
+    reg.register_decorator("derive_Deserialize", ser::expand_derive_deserialize);
+}
